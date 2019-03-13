914b6df4762ecc564b3928ff15520c080c2e09b8
Matheus LESSA <matheus.220@hotmail.com>
2019-03-13 01:06:24 +0100

    Creating the animation_rviz class to merge everything that concerns rviz animation

---
+++ src/animation_rviz_node.cpp
@@ -0,0 +1,25 @@
+#include "animation_rviz.h"
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "animation_rviz_node");
+  ros::NodeHandle nh;
+
+  rosdrone_Animation::animationRviz animation(nh);
+
+  ros::Rate rate(60.0);
+
+  while (ros::ok())
+  {
+    ros::spinOnce();
+
+    animation.addMeasuresArrows();
+    animation.addDesiredArrows();
+    animation.addVelocityArrows();
+
+    animation.publishMarkers();
+
+    rate.sleep();
+  }
+}
--- src/drone_operator_node.cpp
+++ src/drone_operator_node.cpp
@@ -10,13 +10,11 @@ int main(int argc, char** argv)
   ROS_INFO("Main Hover Node Launched");
 
   rosdrone_Detector::targetDetector detector(nhg, nhp);
+  rosdrone::outerLoopRT controller(nhg);
+  rosdrone::commandCreator command(nhg, nhp);
 
-  rosdrone_Controller::outerLoopRT controller(nhg);
-
-  rosdrone_Command::commandCreator command(nhg, nhp);
-
-  ros::Rate rate(15.0);
-  ROS_INFO("Outer loop starting at 15 hz");
+  ros::Rate rate(20.0);
+  ROS_INFO("Outer loop starting at 20 hz");
 
   double time = ros::Time::now().toSec();
 
@@ -32,9 +30,7 @@ int main(int argc, char** argv)
     ros::spinOnce();
 
     detector.spinDetector();
-
     command.spinCommand();
-
     controller.spinControl();
 
     rate.sleep();
